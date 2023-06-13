0e9191e1f6bb11a72c7b0eebc908c3d83f16f4c7
axis.1410 <adityasingh14102001@gmail.com>
2023-06-13 14:53:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import { AddTransaction } from './components/AddTransaction';
+import { Balance } from './components/Balance';
+import { Header } from './components/Header';
+import { IncomeExpense } from './components/IncomeExpense';
+import { TransactionList } from './components/TransactionList';
+
+import { GlobalProvider } from './context/GlobalState';
+
+export const App = () => {
+	return (
+		<>
+			<GlobalProvider>
+				<Header />
+				<div className='container'>
+					<Balance />
+					<IncomeExpense />
+					<TransactionList />
+					<AddTransaction />
+				</div>
+			</GlobalProvider>
+		</>
+	);
+};
+
+export default App;
